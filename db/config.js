e45249dc40e070ce6947dad6d1dcf09430efc8ad
Shantam Shukla <shantamshuklagkp@gmail.com>
2023-02-04 00:35:55 +0530

    hh

--- db/config.js
+++ db/config.js
@@ -1,14 +1,10 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-// mongoose.connect(
-//   "mongodb+srv://username1:123@db.2sdvtqp.mongodb.net/db",
-//   { useNewUrlParser: true }
-// );
-mongoose.connect('mongodb://localhost:27017/course', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log(`CONNECTED TO MONGO!`);
-    })
-    .catch((err) => {
-        console.log(`OH NO! MONGO CONNECTION ERROR!`);
-        console.log(err);
-    })
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
+var conn = mongoose.connection;
+conn.on("connected", function () {
+  console.log("database is connected successfully");
+});
+conn.on("disconnected", function () {
+  console.log("database is disconnected successfully");
+});
+conn.on("error", console.error.bind(console, "connection error:"));
