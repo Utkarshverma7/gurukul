0de80921ce5b9f896d9121f07b0704e03971ba92
Shantam Shukla <shantamshuklagkp@gmail.com>
2023-02-04 12:36:15 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
 require("./db/config");
 const Data = require("./db/Data");
@@ -10,24 +10,32 @@ app.use(
     extended: true,
   })
 );
+app.use(cors());
 app.use(express.json());
-app.use(express.static("public"))
-app.get("/",async(req,res)=>{res.sendFile(__dirname+"/index.html")})
-app.get("/login",async(req,res)=>{res.sendFile(__dirname+"/login.html")})
-app.get("/course",async(req,res)=>{res.sendFile(__dirname+"/course.html")})
-app.post("/login", async (req, res) => {
-    if (req.body.username==="admin" && req.body.password==="password") {
-        res.sendFile(__dirname+"/course.html")
-    } else {
-      res.send({ result: "No user found" });
-    }
-  });
-app.post("/course", async (req, res) => {
-    result=req.body;
-    res.send(result);
-    console.log(result);
+app.use(express.static("public"));
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/login", async (req, res) => {
+  res.sendFile(__dirname + "/login.html");
+});
+app.get("/course", async (req, res) => {
+  res.sendFile(__dirname + "/course.html");
+});
+app.post("/login", async (req, res) => {
+  if (req.body.username === "admin" && req.body.password === "password") {
+    res.sendFile(__dirname + "/course.html");
+  } else {
+    res.send({ result: "No user found" });
+  }
+});
+app.post("/course", async (req, res) => {
+  var courseData = {
+    course: req.body.course,
+    link: req.body.link,
+  };
+  new Data(courseData).save();
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
